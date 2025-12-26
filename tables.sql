8be90441e0db6b118f08c56d4df5e9fd4272b30c
Juluur <duluursemenov90@gmail.com>
2025-12-27 03:58:24 +0900

    Create tables

---
+++ tables.sql
@@ -0,0 +1,20 @@
+create table institutes (
+	id serial Primary key,
+	name varchar(1024) NOT NULL
+);
+
+create table groups (
+	id serial Primary key,
+	name varchar(2048) NOT NULL,
+	short_name varchar(64) NOT NULL UNIQUE,
+	year int,
+	institube_id int not null
+);
+
+create table students (
+	id serial Primary key,
+	surname varchar(512) NOT NULL,
+	name varchar(512) NOT NULL,
+	father_name varchar(512),
+	group_id int not null
+);
