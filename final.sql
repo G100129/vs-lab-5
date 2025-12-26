8396601315579ecec8d345360ec9f33b3cae679a
Juluur <duluursemenov90@gmail.com>
2025-12-27 06:36:59 +0900

    completed lab 5

---
+++ final.sql
@@ -0,0 +1,25 @@
+create table cathedrals (
+	id serial Primary key,
+	name varchar(2048) NOT NULL,
+	chief varchar(512) NOT NULL
+);
+
+alter table groups
+add column cathedral_id int not null;
+
+insert into cathedrals (name, chief)
+values ('Кафедра информационных технологий', 'Васильева Наталья Васильевна'),
+	   ('Математическая экономика и прикладная информатика', 'Матвеева Нюргуяна Николаевна');
+
+select * from cathedrals;
+
+update groups
+set cathedral_id = 1
+where groups.short_name = 'Б-ИВТ-25-1' or groups.short_name = 'Б-ИВТ-25-2' or groups.short_name = 'Б-ФИИТ-25';
+
+update groups
+set cathedral_id = 2
+where groups.short_name = 'Б-ПИ-25-1';
+
+select * from groups
+where groups.cathedral_id = 1;
